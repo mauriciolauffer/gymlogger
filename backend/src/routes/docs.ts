9e854d7ffa7b7f89daa0961176809a004642d888
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2026-09-23 10:41:18 +1000

    fix: lazy load openapi spec

--- backend/src/routes/docs.ts
+++ backend/src/routes/docs.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import { Scalar } from "@scalar/hono-api-reference";
-import openApiSpec from "../../openapi/openapi.json";
+
+let openApiSpec: object;
+
 export const docsRouter = new Hono()
-  .use("*", (c, next) => {
+  .use("*", async (c, next) => {
     if (process.env.NODE_ENV !== "development") {
       return c.json({ error: "Endpoint not found" }, 404);
+    }
+    if (openApiSpec) {
+      openApiSpec = await import("../../openapi/openapi.json");
     }
     return next();
   })
